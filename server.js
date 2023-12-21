d2fdd52546be6e59bb88190377b814275372175c
Vhkan <vhkan@yahoo.com>
2023-12-20 18:39:15 -0700

    remove scss

--- server.js
+++ server.js
@@ -16,14 +16,6 @@ app.set('view engine', 'ejs');
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  '/styles',
-  sassMiddleware({
-    source: __dirname + '/styles',
-    destination: __dirname + '/public/styles',
-    isSass: false, // false => scss, true => sass
-  })
-);
 app.use(express.static('public'));
 
 // Separated Routes for each Resource
