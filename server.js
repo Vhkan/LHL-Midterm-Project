26acf4c9856a3a491c8a24dac0165ea34f99531a
Atkinss1 <jordanatkins@live.com>
2023-12-20 20:53:20 -0700

    delete: duplicate example route

--- server.js
+++ server.js
@@ -37,16 +37,7 @@ const usersRoutes = require('./routes/users');
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
 app.use('/api/users', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
-app.use('/users', usersRoutes);
-// Note: mount other resources here, using the same pattern above
-
-// Home page
-// Warning: avoid creating more routes in this file!
-// Separate them into separate routes files (see above).
-
-app.get('/', (req, res) => {
-  res.render('index');
-});
+app.use('/', usersRoutes);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
