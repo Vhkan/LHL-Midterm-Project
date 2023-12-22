a4104a3f5a9774d3d4d74b042d30a8be59bbdcde
Atkinss1 <jordanatkins@live.com>
2023-12-22 08:24:58 -0700

    add: email/password

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,10 +1,16 @@
-INSERT INTO users (last_name, first_name, phone, country, street, city, province, post_code) VALUES ('Thomsson', 'Kimbra', '368-556-8158', 'Canada', 'Iowa', 'Fredericton', 'New Brunswick', 'E3G');
-INSERT INTO users (last_name, first_name, phone, country, street, city, province, post_code) VALUES ('Malia', 'Margette', '158-933-5627', 'Canada', 'Bartelt', 'Alberton', 'Prince Edward Island', 'P7K');
-INSERT INTO users (last_name, first_name, phone, country, street, city, province, post_code) VALUES ('Hourahan', 'Gareth', '664-692-5152', 'Canada', 'Sutherland', 'Aylmer', 'Ontario', 'N5H');
-INSERT INTO users (last_name, first_name, phone, country, street, city, province, post_code) VALUES ('Windibank', 'Bastian', '294-572-7484', 'Canada', 'High Crossing', 'Bedford', 'Quebec', 'J6W');
-INSERT INTO users (last_name, first_name, phone, country, street, city, province, post_code) VALUES ('Greensted', 'Vitia', '297-762-2821', 'Canada', 'Service', 'Nicolet', 'Quebec', 'J3T');
-INSERT INTO users (last_name, first_name, phone, country, street, city, province, post_code) VALUES ('Fivey', 'Dwain', '291-177-4087', 'Canada', 'Goodland', 'Saskatoon', 'Saskatchewan', 'S7W');
-INSERT INTO users (last_name, first_name, phone, country, street, city, province, post_code) VALUES ('Castellani', 'Billi', '289-866-3343', 'Canada', 'Dahle', 'Wingham', 'Ontario', 'T5K');
-INSERT INTO users (last_name, first_name, phone, country, street, city, province, post_code) VALUES ('Webb-Bowen', 'Jarred', '229-704-9773', 'Canada', 'North', 'Bedford', 'Quebec', 'J6W');
-INSERT INTO users (last_name, first_name, phone, country, street, city, province, post_code) VALUES ('Sheryn', 'Carter', '307-265-5624', 'Canada', 'Stephen', 'Ottawa', 'Ontario', 'J8Y');
-INSERT INTO users (last_name, first_name, phone, country, street, city, province, post_code) VALUES ('Griffoen', 'Kerby', '650-455-9626', 'Canada', 'Calypso', 'Prince Albert', 'Saskatchewan', 'S6W');
+-- TEST ACCOUNT -- EMAIL test@test.com PASSWORD 1234
+INSERT INTO users (last_name, first_name, phone, country, street, city, province, post_code, is_customer, email, password) VALUES ('Dwayne', 'Johnson', '777-555-3233', 'Canada', 'yellow street', 'Alberta', 'T3NW1F', false, 'test@test.com', '1234');
+
+--
+
+
+INSERT INTO users (last_name, first_name, phone, country, street, city, province, post_code, is_customer, email, password) VALUES ('Folshom', 'Shayne', '719-289-7779', 'United States', 'Prairieview', 'Colorado Springs', 'Colorado', '80940', true, 'sfolshom0@omniture.com', '2_R01S');
+INSERT INTO users (last_name, first_name, phone, country, street, city, province, post_code, is_customer, email, password) VALUES ('Washington', 'Bartram', '202-249-5394', 'United States', 'Arizona', 'Washington', 'District of Columbia', '20310', false, 'bwashington1@facebook.com', '0p''ysH');
+INSERT INTO users (last_name, first_name, phone, country, street, city, province, post_code, is_customer, email, password) VALUES ('Fransemai', 'Maryann', '202-250-7438', 'United States', 'Oriole', 'Washington', 'District of Columbia', '20231', false, 'mfransemai2@virginia.edu', '9R=eP?');
+INSERT INTO users (last_name, first_name, phone, country, street, city, province, post_code, is_customer, email, password) VALUES ('Fleetwood', 'Aggie', '210-577-4175', 'United States', 'Burning Wood', 'San Antonio', 'Texas', '78265', false, 'afleetwood3@amazonaws.com', '8ej_');
+INSERT INTO users (last_name, first_name, phone, country, street, city, province, post_code, is_customer, email, password) VALUES ('Miche', 'Edee', '812-746-0530', 'United States', 'Doe Crossing', 'Evansville', 'Indiana', '47725', false, 'emiche4@shareasale.com', '1M5*Lp');
+INSERT INTO users (last_name, first_name, phone, country, street, city, province, post_code, is_customer, email, password) VALUES ('Fosberry', 'Kyle', '719-542-9813', 'United States', 'Northport', 'Colorado Springs', 'Colorado', '80945', false, 'kfosberry5@foxnews.com', '11wuSH');
+INSERT INTO users (last_name, first_name, phone, country, street, city, province, post_code, is_customer, email, password) VALUES ('Oldershaw', 'Ashly', '901-745-6522', 'United States', 'Fremont', 'Memphis', 'Tennessee', '38136', false, 'aoldershaw6@marriott.com', '9ngdd5n9');
+INSERT INTO users (last_name, first_name, phone, country, street, city, province, post_code, is_customer, email, password) VALUES ('Kiledal', 'Adriane', '256-797-7628', 'United States', 'Elmside', 'Gadsden', 'Alabama', '35905', true, 'akiledal7@oaic.gov.au', '7ou4t`');
+INSERT INTO users (last_name, first_name, phone, country, street, city, province, post_code, is_customer, email, password) VALUES ('Prestedge', 'Antonina', '904-229-6167', 'United States', 'Larry', 'Jacksonville', 'Florida', '32209', false, 'aprestedge8@naver.com', '5(/Wn$');
+INSERT INTO users (last_name, first_name, phone, country, street, city, province, post_code, is_customer, email, password) VALUES ('Bilbery', 'Jesse', '323-355-5417', 'United States', 'Lighthouse Bay', 'Long Beach', 'California', '90810', false, 'jbilbery9@independent.co.uk', '8ad8{');
