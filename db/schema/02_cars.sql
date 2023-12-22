205492c44ebca17a59109a8588fb0f01a0972b1e
Atkinss1 <jordanatkins@live.com>
2023-12-22 14:05:54 -0700

    refactor: cars table

--- db/schema/02_cars.sql
+++ db/schema/02_cars.sql
@@ -2,17 +2,14 @@ DROP TABLE IF EXISTS cars CASCADE;
 
 CREATE TABLE cars (
   id SERIAL PRIMARY KEY NOT NULL,
-  serial_number INT NOT NULL,
   make VARCHAR(255) NOT NULL,
   model VARCHAR(255) NOT NULL,
   year INT NOT NULL,
-  interior_color VARCHAR(255) NOT NULL,
-  exterior_color VARCHAR(255) NOT NULL,
-  price INT NOT NULL,
+  color VARCHAR(255) NOT NULL,
   odometer INT NOT NULL,
-  transmission VARCHAR(255) NOT NULL,
+  price INT NOT NULL,
   photo_url_1 VARCHAR(255),
   photo_url_2 VARCHAR(255),
   photo_url_3 VARCHAR(255),
-  is_sold BOOLEAN
+  is_sold BOOLEAN DEFAULT false
 );
