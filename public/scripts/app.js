30fcb47a0f92b532cb2e56342d70d40fbacecac1
Vhkan <vhkan@yahoo.com>
2023-12-26 12:22:10 -0700

    apply more css styling for proper content display on pages

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -100,8 +100,7 @@ function updateCarList(cars) {
   cars.forEach((data) => {
     const html = `
       <div class="col-md-3">
-        <div class="card" style="
-        margin-bottom: 300px;>
+        <div class="card">
           <a href="/sell/${data.id}">
             <img src="${data.photo_url_1}" class="card-img-top" alt="image_unavailable">
           </a>
