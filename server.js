47dae65e3da391655b19bcad970496cd32666e80
Atkinss1 <jordanatkins@live.com>
2024-01-04 19:21:18 -0700

    delete: unused files

--- server.js
+++ server.js
@@ -29,15 +29,12 @@ app.use(express.static('public'));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const userApiRoutes = require('./routes/users-api');
-const widgetApiRoutes = require('./routes/widgets-api');
+
 const usersRoutes = require('./routes/users');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
-app.use('/api/users', userApiRoutes);
-app.use('/api/widgets', widgetApiRoutes);
 app.use('/', usersRoutes);
 
 
